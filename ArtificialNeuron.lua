b0296d2ed566d49323676b4bb38607abffc45de4
IvanTheScript <122665922+IvanTheProtogen@users.noreply.github.com>
2025-02-10 01:51:23 +0300

    Update ArtificialNeuron.lua

--- ArtificialNeuron.lua
+++ ArtificialNeuron.lua
@@ -1,48 +1,40 @@
--- skidded from AI, cuz idk shit about implementing an artificial neuron.
+-- skidded out from AI, cuz idk anything about implementing a neuron lol
+-- well, i added it a little.. so yeah
+
 Neuron = {}
 Neuron.__index = Neuron
 
-function Neuron:new(input_size)
-    local obj = {}
-    setmetatable(obj, self)
-    obj.weights = {}
-    for i = 1, input_size do
-        obj.weights[i] = math.random()
-    end
-    obj.bias = math.random()
-    obj.learning_rate = 0.1
-    return obj
+function Neuron.new(activFunc, weights, bias)
+    local self = setmetatable({}, Neuron)
+    self.activFunc = activFunc
+    self.weights = weights
+    self.bias = bias
+    return self
 end
 
-function Neuron:sigmoid(x) -- oh yeah, this "sigmoid" has been replaced with ReLU by me now
-    return math.max(0,x)
-end
-
-function Neuron:sigmoid_derivative(x) -- same shit
-    if x < 0 then
-        return 0
-    end
-    return 1
-end
-
--- holy shit, ReLU is so fucking simple yet so amazing
-
-function Neuron:feedforward(inputs)
-    local weighted_sum = 0
+function Neuron:activate(inputs)
+    assert(#inputs==#self.weights, "amount of inputs should be equal to amount of weights")
+    local sum = 0
     for i = 1, #inputs do
-        weighted_sum = weighted_sum + (inputs[i] * self.weights[i])
+        sum = sum + inputs[i] * self.weights[i]
     end
-    weighted_sum = weighted_sum + self.bias
-    self.output = self:sigmoid(weighted_sum)
-    return self.output
+    sum = sum + self.bias
+    return self.activFunc(sum)
 end
 
-function Neuron:train(inputs, target_output)
-    local output = self:feedforward(inputs)
-    local error = target_output - output
-    for i = 1, #inputs do
-        self.weights[i] = self.weights[i] + (self.learning_rate * error * self:sigmoid_derivative(output) * inputs[i])
+function Neuron:train(inputs, reward, learningRate, gamma, nextInputs)
+    local output = self:activate(inputs)
+    local target = reward
+    if nextInputs then
+        local nextOutput = self:activate(nextInputs)
+        target = reward + gamma * nextOutput
     end
-    self.bias = self.bias + (self.learning_rate * error * self:sigmoid_derivative(output))
+    local error = target - output
+    for i = 1, #self.weights do
+        self.weights[i] = self.weights[i] + learningRate * error * inputs[i]
+    end
+    self.bias = self.bias + learningRate * error
     return output
 end
+
+return Neuron
