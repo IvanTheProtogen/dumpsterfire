57076cae73ee5ea240b25f61c4fb296292c3cb68
IvanTheScript <122665922+IvanTheProtogen@users.noreply.github.com>
2025-02-11 22:31:07 +0300

    Create GELU_Approximation.lua

---
+++ GELU_Approximation.lua
@@ -0,0 +1,9 @@
+local function gelu(x)
+    return x*(1/(1+math.exp(-x*1.704)))
+end
+local function geluDeriv(x)
+    if x < 0 then
+        return (gelu(x+0.75)*1.704)/2
+    end
+    return -((gelu(-x+0.75)*1.704)/2)
+end
