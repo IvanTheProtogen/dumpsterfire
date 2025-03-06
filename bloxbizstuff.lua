1252b698a42cd859b67cf1a68ff8ce15dcb972d6
IvanTheScript <122665922+IvanTheProtogen@users.noreply.github.com>
2025-03-06 07:46:12 +0300

    Create bloxbizstuff.lua

---
+++ bloxbizstuff.lua
@@ -0,0 +1,142 @@
+local args = {
+    [1] = {
+        ["WalkAnimation"] = 5319909330,
+        ["Accessories"] = {
+            [1] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 12114581505,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Front
+            },
+            [2] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 14103139737,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Back
+            },
+            [3] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 14991646689,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Neck
+            },
+            [4] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 12114570675,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Shoulder
+            },
+            [5] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 15295909184,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Hat
+            },
+            [6] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 12114574600,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Shoulder
+            },
+            [7] = {
+                ["Rotation"] = Vector3.new(-105,180,0),
+                ["AssetId"] = 10095627733,
+                ["Position"] = Vector3.new(-1.1, 1, -0.8),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Waist
+            },
+            [8] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 607702162,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Hat
+            },
+            [9] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 11714797755,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Hat
+            },
+            [10] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 7485974573,
+                ["Position"] = Vector3.new(0, 0, 0),
+                ["Scale"] = Vector3.new(1, 1, 1),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Waist
+            },
+            [11] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 124899455887488,
+                ["Position"] = Vector3.new(0.25, 0, -0.2),
+                ["Scale"] = Vector3.new(2/3, 2/3, 2/3),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Waist
+            },
+            [12] = {
+                ["Rotation"] = Vector3.new(0, 0, 0),
+                ["AssetId"] = 98783947166372,
+                ["Position"] = Vector3.new(-0.25, 0, -0.2),
+                ["Scale"] = Vector3.new(2/3, 2/3, 2/3),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Waist
+            },
+            [13] = {
+                ["Rotation"] = Vector3.new(65, 0, 0),
+                ["AssetId"] = 15937580696,
+                ["Position"] = Vector3.new(0, -0.2, 0),
+                ["Scale"] = Vector3.new(1.5, 1.5, 1.5),
+                ["IsLayered"] = false,
+                ["AccessoryType"] = Enum.AccessoryType.Face
+            }
+        },
+        ["RightLegColor"] = nil --[[Color3]],
+        ["MoodAnimation"] = 0,
+        ["LeftLegColor"] = nil --[[Color3]],
+        ["JumpAnimation"] = 5319917561,
+        ["ProportionScale"] = 1,
+        ["BodyTypeScale"] = 0.30000001192092896,
+        ["ClimbAnimation"] = 5319931619,
+        ["LeftArmColor"] = nil --[[Color3]],
+        ["Face"] = 0,
+        ["Pants"] = 11752024081,
+        ["RightArmColor"] = nil --[[Color3]],
+        ["DepthScale"] = 1,
+        ["WidthScale"] = 1,
+        ["SwimAnimation"] = 5319927054,
+        ["RightArm"] = 0,
+        ["GraphicTShirt"] = 0,
+        ["Head"] = 0,
+        ["Shirt"] = 11752023381,
+        ["RightLeg"] = 0,
+        ["RunAnimation"] = 5319900634,
+        ["Torso"] = 48474356,
+        ["FallAnimation"] = 5319914476,
+        ["TorsoColor"] = nil --[[Color3]],
+        ["IdleAnimation"] = 5319922112,
+        ["LeftArm"] = 0,
+        ["HeadScale"] = 1,
+        ["HeightScale"] = 1,
+        ["HeadColor"] = nil --[[Color3]],
+        ["LeftLeg"] = 0
+    }
+}
+
+pcall(function()game:GetService("ReplicatedStorage").BloxbizRemotes.CatalogOnApplyOutfit:FireServer(unpack(args))end)
